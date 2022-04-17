b2f13e32976eb2d5970641dedcb2aa864369145a
mario-estevam <mario.neto47@gmail.com>
2022-04-17 12:43:38 -0300

    Aula 11 - Implementando interface em EJS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const rotaPost = require('./routes/posts.rota')
 
 var expressLayouts = require('express-ejs-layouts')
 
+const indexRoute = require('./routes/index.rota')
+
+
+
 const app = express()
 
 app.use(express.json())
@@ -15,36 +19,11 @@ app.use(expressLayouts)
 
 app.use('/static', express.static('public'))
 
-app.use('/usuarios', rotaUsuario)
-app.use('/posts', rotaPost)
+app.use('/api/usuarios', rotaUsuario)
+app.use('/api/posts', rotaPost)
+app.use('/', indexRoute)
 
-app.get('/', (req, res) => {
-    res.json({msg: "Hello from Express!"})
-})
 
-app.get('/cursos', (req, res) => {
-    const cursos = [
-            {nome: "Programação fronend", ch: 280}, 
-            {nome: "Programação backend", ch: 330},
-            {nome: "Programação concorrente", ch: 300},
-            {nome: "Programação distribuída", ch: 400}
-    ]
-    res.render('pages/cursos/index', {cursos: cursos})
-})
-
-app.get('/alunos', (req, res) => {
-    const alunos = [
-            {nome: "João Pedro"},
-            {nome: "Fernanda"},
-            {nome: "Francisco"}
-    ]
-    res.render('pages/alunos/index', {alunos: alunos})
-})
-
-app.get('/home', (req, res) => {
-    const number = Math.random()
-    res.render('pages/index', {variavel: number})
-})
 
 app.listen(8080, () => {
     console.log('Servidor pronto na porta 8080')
