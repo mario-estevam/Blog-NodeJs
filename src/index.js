3afffc81c40fc44677213afdb82ef9873faac9e6
mario-estevam <mario.neto47@gmail.com>
2022-04-17 11:02:39 -0300

    Aula 10 - View Engines

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const rotaUsuario = require('./routes/usuario.rota')
 const rotaPost = require('./routes/posts.rota')
 
+var expressLayouts = require('express-ejs-layouts')
+
 const app = express()
+
 app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.set('layout', 'layouts/layout')
+
+app.use(expressLayouts)
 
 app.use('/static', express.static('public'))
 
@@ -12,6 +20,30 @@ app.use('/posts', rotaPost)
 
 app.get('/', (req, res) => {
     res.json({msg: "Hello from Express!"})
+})
+
+app.get('/cursos', (req, res) => {
+    const cursos = [
+            {nome: "Programação fronend", ch: 280}, 
+            {nome: "Programação backend", ch: 330},
+            {nome: "Programação concorrente", ch: 300},
+            {nome: "Programação distribuída", ch: 400}
+    ]
+    res.render('pages/cursos/index', {cursos: cursos})
+})
+
+app.get('/alunos', (req, res) => {
+    const alunos = [
+            {nome: "João Pedro"},
+            {nome: "Fernanda"},
+            {nome: "Francisco"}
+    ]
+    res.render('pages/alunos/index', {alunos: alunos})
+})
+
+app.get('/home', (req, res) => {
+    const number = Math.random()
+    res.render('pages/index', {variavel: number})
 })
 
 app.listen(8080, () => {
